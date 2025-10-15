7ba647ce3e086fe039b5c7c74cb6dd5f70e8c90f
inirav <niravpanchalmerai@gmail.com>
2025-10-15 11:22:15 +0530

    final implementation

--- inference.sh
+++ inference.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-MODEL_PATH="$HOME/projects/paligemma-weights/paligemma-3b-pt-224"
-PROMPT="this building is "
-IMAGE_FILE_PATH="test_images/pic1.jpeg"
+MODEL_PATH="/home/niravpanchal/Documents/paligemma-3b-pt-224"
+PROMPT="what is dog holding in mouth? "
+IMAGE_FILE_PATH="dog.jpeg"
 MAX_TOKENS_TO_GENERATE=100
 TEMPERATURE=0.8
 TOP_P=0.9
 DO_SAMPLE="False"
-ONLY_CPU="False"
+ONLY_CPU="True"
 
-python inference.py \
+uv run inference.py \
     --model_path "$MODEL_PATH" \
     --prompt "$PROMPT" \
     --image_file_path "$IMAGE_FILE_PATH" \
